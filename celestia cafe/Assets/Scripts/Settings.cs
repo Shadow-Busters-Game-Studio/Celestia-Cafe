f8674b46118f8f08abf9677b796cbeaa0acc9c7d
manon lefebvre <manonalefebvre+github@gmail.com>
2022-12-16 13:11:00 -0800

    I ADDED AUTO AND SKIP BUTTON BUT YARNSPINNER WONT LET ME CONNECT THEM TO THE BUTTON PRESSES

--- celestia cafe/Assets/Scripts/Settings.cs
+++ celestia cafe/Assets/Scripts/Settings.cs
@@ -1,21 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SaveName(string name){
         PlayerPrefs.SetString("Player Name", name);
     }
